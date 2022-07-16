c4ba843cf970daec8bae374b03cb74ef7bcd4c27
Danilo <simao22danilo@gmail.com>
2022-07-16 07:21:41 -0400

    Debugando Erros

---
+++ validaArrays.js
@@ -0,0 +1,30 @@
+function validaArrays(arr, num) {
+  try {
+    if (!arr && !num) throw new ReferenceError("Envie os parâmetros!");
+
+    if (typeof arr !== "object")
+      throw new TypeError("Envie um elemento do tipo Array!");
+
+    if (typeof num !== "number")
+      throw new TypeError("Envie um elemento do tipo Number!");
+
+    if (arr.length !== num) throw new RangeError("Tamanho do array inválido!");
+
+    return arr;
+  } catch (e) {
+    if (e instanceof RangeError) {
+      console.log("RangeError!");
+      console.log(e.stack);
+    } else if (e instanceof ReferenceError) {
+      console.log("ReferenceError!");
+      console.log(e.stack);
+    } else if (e instanceof TypeError) {
+      console.log("Outro tipo de erro!");
+      console.log(e.stack);
+    } else {
+      console.log("Tipo de erro não esperado:" + e);
+    }
+  }
+}
+
+console.log(validaArrays([1, 2, 3, 4, 5], 5));
